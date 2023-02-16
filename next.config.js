9235e0570a79b68aeba41c2273c5798ce9ba33f0
Kasım Şahin <kasim.sahin@pixelate.com.tr>
2023-02-17 02:35:00 +0300

    fix: added necessary packages and style files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "@/styles/config/index.scss";`,
+  },
+ 
 }
 
 module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import '@/styles/fonts.scss';
+import '@/styles/globals.scss';
 
 export default function RootLayout({
   children,
